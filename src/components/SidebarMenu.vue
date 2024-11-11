e32914bda5adef00e437ce95d5600dc61127f374
青柠 <294398937@qq.com>
2024-11-11 21:18:35 +0800

    order table

---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { } from "vue"
+</script>
+
+<template>
+ <div >
+    <el-menu
+       default-active="2"
+       class="el-menu-vertical-demo"
+    >
+       <el-sub-menu index="1">
+          <template #title>
+             <el-icon><Suitcase /></el-icon>
+             <span>报备管理</span>
+          </template>
+
+          <el-menu-item index="1-1">初报查询</el-menu-item>
+          <el-menu-item index="1-2">续报查询</el-menu-item>
+          <el-menu-item index="1-3">结案查询</el-menu-item>
+       </el-sub-menu>
+       <el-menu-item>
+          <template #title>
+             <el-icon><User /></el-icon>
+             <span>用户管理</span>
+          </template>
+                            
+       </el-menu-item>
+       <el-menu-item index="4">
+          <el-icon><setting /></el-icon>
+          <span>系统设置</span>
+       </el-menu-item>
+    </el-menu>
+ </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/views/admin/home.vue
+++ src/views/admin/home.vue
@@ -4,6 +4,8 @@ import { ref } from 'vue'
 // 使用本地图片的导入方法
 import logoImg from '@/assets/img/logo.png'
 
+import SidebarMenu from '@/components/SidebarMenu.vue';
+
 </script>
 
 <template>
@@ -11,16 +13,47 @@ import logoImg from '@/assets/img/logo.png'
       <div class="sidebar">
          <div class="logo-container">
             <div class="logo">
-               <el-avatar shape="square" :src="logoImg"></el-avatar>
+               <el-avatar size="small" shape="square" :src="logoImg"></el-avatar>
             </div>
             <div class="logo-text">
-              <h2>后台管理</h2> 
+              <h3>KCRS后台管理</h3> 
             </div>
          </div>
+         <div class="menu-container">
+           <sidebar-menu /> 
+         </div>
+         
       </div>
       <div class="main">
-         <div class="header">11</div>
-         <div class="content">22</div>
+         <div class="header">
+            <div class="header-left">
+               <el-icon :size="30" color="#515151"><Fold /></el-icon>
+            </div>
+            <div class="header-middle"></div>
+            <div class="header-right">
+               <el-avatar :src="logoImg" />
+            </div>
+         </div>
+         <div class="content">
+            <div class="query"></div>
+            <div class="table">
+               <el-table border style="width: 98%;">
+                  <el-table-colunm prop="area" label="区域"></el-table-colunm>
+                  <el-table-colunm prop="name" label="名称"></el-table-colunm>
+                  <el-table-colunm prop="contact" label="联系人"></el-table-colunm>
+                  <el-table-colunm prop="phone" label="电话"></el-table-colunm>
+                  <el-table-colunm prop="position" label="职位"></el-table-colunm>
+                  <el-table-colunm prop="models" label="机型"></el-table-colunm>
+                  <el-table-colunm prop="address" label="地址"></el-table-colunm>
+                  <el-table-colunm  label="操作" fixed="right">
+                     <template #default>
+                        <el-button link type="primary" size="small">详情</el-button>
+                        <el-button link type="primary" size="small">审核</el-button>
+                     </template>
+                  </el-table-colunm>
+               </el-table>
+            </div>
+         </div>
       </div>
 
       
